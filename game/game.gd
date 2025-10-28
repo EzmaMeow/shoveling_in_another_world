1e2b3ae90e70260653c8fb7b7959cc8423c1db74
Ezma Green <EzmaMeow@hotmail.com>
2025-10-28 01:59:58 -0400

    minor update
    Added a playable area scene to make adding a playable zone easier in 2d without depending on tilemaps. It is a rectangle that has a background, toggle borders, and an area2d to detects if physics bodies enter or exit the area. It is a static body for now, though that makes the naming misleading despite it acts like an area as well as a static body. I may change since it may make use of the area signals more than the static body or I could make it a node2d and add more properties (I rather not).

--- game/game.gd
+++ game/game.gd
@@ -8,6 +8,8 @@ extends Node
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	if Engine.is_editor_hint(): 
+		return
 	if event.is_action("accept"):
 		#get_viewport().set_input_as_handled()
 		pass
@@ -15,23 +17,23 @@ func _unhandled_input(event: InputEvent) -> void:
 		#get_viewport().set_input_as_handled()
 		pass
 	if event.is_action("up"):
-		player_controller.jump.emit(event.get_action_strength("up"))
+		player_controller.up.emit(event.get_action_strength("up"))
 		get_viewport().set_input_as_handled()
 	if event.is_action("down"):
-		player_controller.crouch.emit(event.get_action_strength("down"))
+		player_controller.down.emit(event.get_action_strength("down"))
 		get_viewport().set_input_as_handled()
 	if event.is_action("sprint"):
 		player_controller.sprint.emit(event.get_action_strength("sprint"))
 		get_viewport().set_input_as_handled()
 		
+func update_globals():
+	if (get_viewport().get_camera_2d()):
+		RenderingServer.global_shader_parameter_set("world_pos_2D", get_viewport().get_camera_2d().get_screen_center_position())
+		
 func _physics_process(delta: float) -> void:
+	update_globals()
+	if Engine.is_editor_hint(): 
+		return
 	var input_direction = Vector2(Input.get_axis("left","right"),Input.get_axis("forward","back"))
 	player_controller.move_direction = input_direction
 	#print(get_viewport().get_camera_2d().global_position)
-	if (get_viewport().get_camera_2d()):
-		#RenderingServer.global_shader_parameter_set("WORLD_POS_2D", get_viewport().get_camera_2d().position)
-		RenderingServer.global_shader_parameter_set("world_pos_2D", get_viewport().get_camera_2d().get_screen_center_position())
-		#RenderingServer.global_shader_parameter_set("world_pos_2D", get_viewport().get_camera_2d().global_position)
-		#RenderingServer.global_shader_parameter_set("viewport_size", get_window().size)
-		#RenderingServer.global_shader_parameter_set("world_pos_2D", get_viewport().get_visible_rect().position)
-		RenderingServer.global_shader_parameter_set("viewport_size", get_viewport().get_visible_rect().size*get_viewport().get_camera_2d().zoom)
---
+++ game/scenes/playable_area_2d.gd
@@ -0,0 +1,90 @@
+@tool
+class_name Playable_Area_2d extends Node2D
+enum BORDERS { NORTH = 1, SOUTH = 2, EAST = 4, WEST = 8 }
+##Emited when a physics body enter the area bounds
+signal body_entered(body:Node2D)
+##Emited when a physics body exit the area bounds
+signal body_exit(body:Node2D)
+##Emited when a the area size changed. Will only be called if there a change.
+signal size_changed(size:Vector2)
+
+@export var size: Vector2 = Vector2(640,640):
+	set(value):
+		if (size == value):
+			return
+		size = value
+		update_size()
+		size_changed.emit(size)
+
+@export_group('Backgroud')
+@export var show_background : bool = true :
+	set(value):
+		show_background = value
+		%Background.visible = show_background
+		
+@export var background_texture : Texture2D :
+	set(value):
+		if (background_texture == value):
+			return
+		background_texture = value
+		%Background.texture = background_texture
+
+##The material the background will use
+##Note: only can take canvas item or shader materials.
+@export var background_material : Material  :
+	set(value):
+		if (background_material == value):
+			return
+		background_material = value
+		%Background.material = background_material
+		
+@export_group('Borders')
+@export_flags('north','south','east','west') var open_borders : int :
+	set(value):
+		if (open_borders == value):
+			return
+		open_borders = value
+		update_borders()
+		
+@export_group('Bounds')
+@export_flags_2d_physics var bounds_collsion_layer : int :
+	set(value):
+		bounds_collsion_layer = value
+		%Bounds.collision_layer = bounds_collsion_layer
+@export_flags_2d_physics var bounds_collsion_mask : int :
+	set(value):
+		bounds_collsion_mask = value
+		%Bounds.collision_mask = bounds_collsion_mask
+
+		
+func update_borders():
+	%NorthBorder.disabled = open_borders & BORDERS.NORTH
+	%SouthBorder.disabled = open_borders & BORDERS.SOUTH
+	%EastBorder.disabled = open_borders & BORDERS.EAST
+	%WestBorder.disabled = open_borders & BORDERS.WEST
+	
+func update_size():
+	if (get_child_count()==0):
+		return
+	%Background.position = -size
+	%Background.size = size*2
+	
+	%BoundsShape.shape.size = size*2
+	
+	%SouthBorder.position.y = size.y
+	%EastBorder.position.x = size.x
+	%WestBorder.position.x = -size.x
+	%NorthBorder.position.y = -size.y
+	
+	
+func _ready() -> void:
+	update_size()
+	update_borders()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	body_entered.emit(body)
+
+
+func _on_body_exited(body: Node2D) -> void:
+	body_entered.emit(body)
